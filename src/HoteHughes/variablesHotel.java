5e1b440d67049031290c667246fd69efb85938c1
Mariana Torres <martorres612@gmail.com>
2023-06-17 09:50:57 -0600

    Asignando valores a variables para el proyecto del Hotel en Java

---
+++ src/HoteHughes/variablesHotel.java
@@ -0,0 +1,31 @@
+package HoteHughes;
+
+public class variablesHotel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//byte
+		byte numPisos = 10;
+		
+		//short
+		short numHabitaciones = 180;
+		
+		//int
+		int huespuedesAnuales = 5679;
+				
+		//long
+		long huespuedesMundiales = 345678267l;
+		
+		//float
+		float costoHabitacion = 4999.99f;
+		
+		//double
+		double costoSuite = 149999.99;
+		
+		//boolean
+		boolean habitacionLibre = true;
+
+	}
+
+}
